581639ad79cff7e645361eee3e53684215b84da8
heloísa <heloisa.paixao@aluno.senai.br>
2026-06-22 08:48:16 -0300

    alterações p/ multer (22/06/2026)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const routes = require('./routes'); 
+const routes = require("./routes");
+const path = require("path"); // Manipula os diretórios.
 
 // Middlewares globais
 app.use(cors()); // Habilita o CORS para permitir requisições do frontend
 app.use(express.json());
 
 // Registro de todas as rotas da API centralizadas
-app.use('/', routes);
+app.use("/", routes);
+
+app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
 
 module.exports = app;
